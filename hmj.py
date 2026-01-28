cb587e3fc401812eeef9a77f45ac572bf3f2f07a
Blaine Mooers <bmooers1@gmail.com>
2026-01-27 21:18:02 -0600

    Updated format to accommodate additional year.

--- hmj.py
+++ hmj.py
@@ -1,40 +1,54 @@
 #!/opt/homebrew/bin/python3.11
-# Universal
 import os
-import calmap
 import calplot
 import pandas as pd
-import numpy as np
-from vega_datasets import data as vds
 import matplotlib.pyplot as plt
 import sqlite3
 
-conn4=sqlite3.connect('/Users/blaine/6003TimeTracking/cb/mytime.db')
+conn4 = sqlite3.connect('/Users/blaine/6003TimeTracking/cb/mytime.db')
 
-# Specific to subsections of the projects
-# FROM newTimeSpent WHERE ProjectID LIKE '[1-9][1-9][1-9]g' Failed
-# FROM newTimeSpent where ProjectID BETWEEN 001 and 999
-## Manuscripts
-datam = pd.read_sql( '''
-SELECT DateDashed, ProjectID, SUM(TimeHr) TimeHr
-FROM newTimeSpent where ProjectID BETWEEN 001 and 999
-GROUP BY DateDashed
-''',conn4)
+# Manuscripts (ProjectID 001-999)
+datam = pd.read_sql('''
+    SELECT DateDashed, ProjectID, SUM(TimeHr) TimeHr
+    FROM zTimeSpent 
+    WHERE ProjectID BETWEEN 001 AND 999
+    GROUP BY DateDashed
+''', conn4)
+
 datam['Date'] = pd.to_datetime(datam['DateDashed'])
 datam = datam.set_index('Date')
-isinstance(datam.index, pd.DatetimeIndex)
 GhrPerDay = pd.Series(datam.TimeHr)
-calplot.calplot(data=GhrPerDay, cmap='Blues', vmin=0.0, how=None, figsize=(16,10));
-plt.suptitle('Manuscript Writing Effort', y=1.0, fontsize=20);
-#plt.savefig('hmj.png', dpi=300, w)
+
+# Create the calendar plot with adjusted figure size
+fig, ax = calplot.calplot(
+    data=GhrPerDay, 
+    cmap='Blues', 
+    vmin=0.0, 
+    how=None, 
+    figsize=(16, 10),
+    colorbar=False  # Disable default colorbar
+)
+
+plt.suptitle('Manuscript Writing Effort', y=0.98, fontsize=20)
+
+# Adjust margins to make room for colorbar on far right
+plt.subplots_adjust(left=0.06, right=0.85, top=0.94, bottom=0.05)
+
+# Add colorbar manually with custom position
+sm = plt.cm.ScalarMappable(cmap='Blues', norm=plt.Normalize(vmin=0, vmax=GhrPerDay.max()))
+sm.set_array([])
+
+# Position: [left, bottom, width, height] in figure coordinates
+cbar_ax = fig.add_axes([0.88, 0.15, 0.02, 0.7])
+cbar = fig.colorbar(sm, cax=cbar_ax)
+cbar.set_label('Hours', fontsize=12)
+
 # Remove old version and write updated version
 strFile = "/Users/blaine/6003TimeTracking/cb/hmj.png"
 if os.path.isfile(strFile):
-   os.remove(strFile)   # Opt.: os.system("rm "+strFile)
-plt.savefig(strFile, dpi=300)
+    os.remove(strFile)
 
+plt.savefig(strFile, dpi=300, bbox_inches='tight')
 plt.show()
 
-
-
-
+conn4.close()
